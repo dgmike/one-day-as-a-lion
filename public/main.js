8e74ebff4cde180e4979ea936cae706c2f6052c6
Michael Granados <michaelgranados@gmail.com>
2015-01-08 09:34:14 -0200

    Creating and using generic modal
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- public/main.js
+++ public/main.js
@@ -1,32 +1,12 @@
+/*jslint browser:true, indent: 2*/
+/*global modal*/
+
 (function ($) {
   "use strict";
-  var addEntrance, resetModal, removeActions, closeModal, addEntranceAction, addEntranceCancelAction;
-
-  resetModal = function () {
-    $('.modal')
-      .find('.header, .content, .action .button')
-      .html('');
-  };
-
-  removeActions = function () {
-    $('.modal .negative.button')
-      .off('click', addEntranceCancelAction);
-    $('.modal .positive.button')
-      .off('click', addEntranceAction);
-  };
-
-  closeModal = function () {
-    removeActions();
-    setTimeout(resetModal, 500);
-  }
-
-  addEntranceCancelAction = function () {
-    closeModal();
-  };
+  var addEntrance, addEntranceAction;
 
   addEntranceAction = function () {
     // TODO submit form
-    closeModal();
   };
 
   addEntrance = function (event) {
@@ -37,21 +17,15 @@
     addEntranceForm = $('#add_entrance_form').clone();
     addEntranceForm.removeAttr('id');
 
-    $('.modal .header').text('Adicionar Entrada');
-
-    $('.modal .content').html(addEntranceForm);
-
-    $('.modal .actions .negative.button')
-      .text('Oops! Cancele minha ação')
-      .on('click', addEntranceCancelAction);
-
-    $('.modal .actions .positive.button')
-      .text('Adicionar entrada!')
-      .on('click', addEntranceAction);
-
-    $('.modal')
-      .modal('setting', 'closable', false)
-      .modal('show');
+    modal.show({
+      title: 'Adicionar Entrada',
+      content: addEntranceForm,
+      cancel: 'Oops! Cancele minha ação',
+      ok: 'Adicionar entrada!',
+      action: {
+        ok: addEntranceAction
+      }
+    });
   };
 
   $('#add-entrance').on('click', addEntrance);
---
+++ public/modal.js
@@ -0,0 +1,96 @@
+/*jslint browser:true, indent: 2*/
+
+(function ($) {
+  "use strict";
+  var Modal, wrapButtonAction;
+
+  // private
+  wrapButtonAction = function (event) {
+    var action, data;
+
+    data = event.data;
+
+    if (data.actions.ok) {
+      data.modalElement.find('.actions .positive.button').off('click', data.actions.ok);
+    }
+    if (data.actions.cancel) {
+      data.modalElement.find('.actions .negative.button').off('click', data.actions.cancel);
+    }
+
+    action = data.actions[data.action];
+    action.apply(this, [].slice.call(arguments));
+  };
+
+
+  // public
+  Modal = function () {
+    this.modalElement = $('.modal');
+
+    this.defaultOptions = {
+      'title': '',
+      'content': '',
+      'ok': '',
+      'cancel': '',
+      'action': {
+        'ok': false,
+        'cancel': false
+      },
+      'modal': {
+        'closable': false
+      }
+    };
+  };
+
+  Modal.prototype.show = function (options) {
+    var modalElement;
+
+    modalElement = this.modalElement;
+
+    if (!options) {
+      options = {};
+    }
+    options = $.extend(true, {}, this.defaultOptions, options);
+
+    modalElement.find('.header').text(options.title);
+    modalElement.find('.content').html(options.content);
+
+    if (false === options.ok) {
+      modalElement.find('.actions .positive.button').text('').hide();
+    } else {
+      modalElement.find('.actions .positive.button').text(options.ok);
+    }
+
+    if (false === options.cancel) {
+      modalElement.find('.actions .negative.button').text('').hide();
+    } else {
+      modalElement.find('.actions .negative.button').text(options.cancel);
+    }
+
+    if (options.action.ok) {
+      modalElement.find('.actions .positive.button')
+        .on('click', {
+          modalElement: modalElement,
+          actions: options.action,
+          action: 'ok'
+        }, wrapButtonAction);
+    }
+
+    if (options.action.cancel) {
+      modalElement.find('.actions .negative.button')
+        .on('click', {
+          modalElement: modalElement,
+          actions: options.action,
+          action: 'cancel'
+        }, wrapButtonAction);
+    }
+
+    $.each(options.modal, function (value, key) {
+      modalElement.modal('setting', key, value);
+    });
+
+    modalElement.modal('show');
+  };
+
+  window.modal = new Modal();
+
+}(window.jQuery));
