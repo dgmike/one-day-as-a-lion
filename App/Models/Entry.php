cf510f364bfd0174fb9be469613f7511cc289052
Michael Granados <michaelgranados@gmail.com>
2015-01-19 23:05:45 -0200

    Values can be negative ;)
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- App/Models/Entry.php
+++ App/Models/Entry.php
@@ -31,13 +31,13 @@ class Entry
 	{
 		$entranceValidator = V::attribute('day', V::notEmpty()->int()->between(1, 31, true))
 							  ->attribute('description', V::notEmpty()->string())
-							  ->attribute('estimated', V::notEmpty()->numeric()->positive())
+							  ->attribute('estimated', V::notEmpty()->numeric())
 							  ->attribute('status', V::notEmpty()->int()->in(array(1, 2)));
 
 		if (2 == $this->status) {
-			$entranceValidator->attribute('real', V::notEmpty()->numeric()->positive());
+			$entranceValidator->attribute('real', V::notEmpty()->numeric());
 		} else {
-			$entranceValidator->attribute('real', V::numeric()->positive());
+			$entranceValidator->attribute('real', V::numeric());
 		}
 
 		$entranceValidator->assert((object) $this->asArray());
